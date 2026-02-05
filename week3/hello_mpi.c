c17e749414d6a53a904fe6f53644fb400a45c8f6
Hugh <hugh94333@users.noreply.github.com>
2026-02-05 15:02:27 +0000

    hello world (MPI/serial)

--- week3/hello_mpi.c
+++ week3/hello_mpi.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, char **argv)
+int main(int argc, char **argv) 
 {
-    int my_rank = -1, uni_size = -1;
+	// declare and initialise error handling variable
+	int ierror = 0;
+	
+	// declare and initialise rank and size varibles
+	int my_rank, uni_size;
+	my_rank = uni_size = 0;
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &uni_size);
+	// intitalise MPI
+	ierror = MPI_Init(&argc, &argv);
 
-    printf("Hello, I am %d of %d\n", my_rank, uni_size);
-    return 0;
+	// gets the rank and world size
+	ierror = MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+	ierror = MPI_Comm_size(MPI_COMM_WORLD,&uni_size);
+
+	// prints to the screen
+	printf("Hello, I am %d of %d\n",my_rank, uni_size);
+
+	// finalise MPI
+	ierror = MPI_Finalize();
+	return 0;
 }
-

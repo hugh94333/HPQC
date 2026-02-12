49321b5b74d2c77530c8de422b8cd0490cf3e03a
Hugh <hugh94333@users.noreply.github.com>
2026-02-12 15:13:28 +0000

    test of combination of MPI_send varieties

---
+++ week4/src/comm_MPI_sendtest.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+// Function Prototypes
+void initialise_mpi(int *argc, char ***argv);
+void get_rank_and_size(int *rank, int *size);
+void root_process(int world_size);
+void worker_process(int my_rank, int world_size);
+void finalise_mpi(void);
+
+// Main Program
+int main(int argc, char **argv)
+{
+    int my_rank = 0;
+    int world_size = 0;
+
+    initialise_mpi(&argc, &argv);
+    get_rank_and_size(&my_rank, &world_size);
+
+    if (world_size > 1)
+    {
+        if (my_rank == 0)
+            root_process(world_size);
+        else
+            worker_process(my_rank, world_size);
+    }
+    else
+    {
+        printf("Unable to communicate with less than 2 processes. "
+               "MPI communicator size = %d\n", world_size);
+    }
+
+    finalise_mpi();
+    return 0;
+}
+
+// Initiallise MPI environment
+void initialise_mpi(int *argc, char ***argv)
+{
+    MPI_Init(argc, argv);
+}
+
+// Obtain rank and communicator size
+void get_rank_and_size(int *rank, int *size)
+{
+    MPI_Comm_rank(MPI_COMM_WORLD, rank);
+    MPI_Comm_size(MPI_COMM_WORLD, size);
+}
+
+// Root process receives four messages from each worker
+void root_process(int world_size)
+{
+    int recv_message;
+    int count = 1;
+    int tag = 0;
+    MPI_Status status;
+
+    for (int source = 1; source < world_size; source++)
+    {
+        for (int i = 1; i <= 4; i++) // four messages per worker
+        {
+            MPI_Recv(&recv_message, count, MPI_INT, source, tag,
+                     MPI_COMM_WORLD, &status);
+
+            printf("Root received message %d: %d from Rank %d\n",
+                   i, recv_message, source);
+        }
+    }
+}
+
+// Worker processes send four messages using different MPI send variants
+void worker_process(int my_rank, int world_size)
+{
+    int count = 1;
+    int tag = 0;
+    int destination = 0;
+    int message;
+    MPI_Status status;
+    MPI_Request request;
+
+    // Synchronous Send
+    message = my_rank * 1;
+    MPI_Ssend(&message, count, MPI_INT, destination, tag, MPI_COMM_WORLD);
+    printf("Rank %d sent MPI_Ssend: %d\n", my_rank, message);
+
+    // Buffered Send
+    int bufsize = MPI_BSEND_OVERHEAD + sizeof(int);
+    void *buffer = malloc(bufsize);
+    MPI_Buffer_attach(buffer, bufsize);
+
+    message = my_rank * 10;
+    MPI_Bsend(&message, count, MPI_INT, destination, tag, MPI_COMM_WORLD);
+    printf("Rank %d sent MPI_Bsend: %d\n", my_rank, message);
+
+    MPI_Buffer_detach(&buffer, &bufsize);
+    free(buffer);
+
+    // Ready send
+    message = my_rank * 100;
+    MPI_Rsend(&message, count, MPI_INT, destination, tag, MPI_COMM_WORLD);
+    printf("Rank %d sent MPI_Rsend: %d\n", my_rank, message);
+
+    // non blocking send
+    message = my_rank * 1000;
+    MPI_Isend(&message, count, MPI_INT, destination, tag, MPI_COMM_WORLD, &request);
+    MPI_Wait(&request, MPI_STATUS_IGNORE);
+    printf("Rank %d sent MPI_Isend: %d\n", my_rank, message);
+}
+
+// Finalise MPI environment
+void finalise_mpi(void)
+{
+    MPI_Finalize();
+}
